fad2bd5cd609d826e01488114e688cb12ed06288
Chirath0627 <chamu0627@gmail.com>
2025-07-27 08:59:23 +0530

    removed fgfgfgf

--- admindash.php
+++ admindash.php
@@ -95,7 +95,6 @@ include 'db_connection.php';
     <script>
         function showSection(sectionId) {
             let sections = document.querySelectorAll('.content-section');
-            //fgfgfgf
             sections.forEach(section => section.style.display = 'none');
             document.getElementById(sectionId).style.display = 'block';
         }
